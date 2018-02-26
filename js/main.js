c26f4d17ca4fd30e69c74b68fd243819c0700241
Tatiana Passarella <siouxie77@tiscali.it>
2018-02-26 13:28:57 +0100

    First approach to design style

--- js/main.js
+++ js/main.js
@@ -157,6 +157,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.tabIndex = 3;
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
